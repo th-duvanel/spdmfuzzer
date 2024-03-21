ba9bb621938a3cd98f78b5cb2446c32a30233b71
th-duvanel <thiago.duvanel.ferreira@live.com>
2024-03-20 23:01:09 -0300

    Added NEG_ALGORITHMS and ALGORITHMS

--- PCAPs/SPDMoverMCTP.lua
+++ PCAPs/SPDMoverMCTP.lua
@@ -50,29 +50,11 @@ local reqres_types = {
         [0x82] = "Request: GET_CERTIFICATE", -- 1.0.2
         [0x83] = "Request: CHALLENGE", -- 1.0.2
         [0x84] = "Request: GET_VERSION", -- 1.0.2
-        [0x85] = "Request: CHUNK_SEND",
-        [0x86] = "Request: CHUNK_GET",
         [0x87] = "Request: GET_ENDPOINT_INFO",
         [0xE0] = "Request: GET_MEASUREMENTS", -- 1.0.2
         [0xE1] = "Request: GET_CAPABILITIES", -- 1.0.2
         [0xE2] = "Request: GET_SUPPORTED_EVENT_TYPES",
         [0xE3] = "Request: NEGOTIATE_ALGORITHMS", -- 1.0.2
-        [0xE4] = "Request: KEY_EXCHANGE",
-        [0xE5] = "Request: FINISH",
-        [0xE6] = "Request: PSK_EXCHANGE",
-        [0xE7] = "Request: PSK_FINISH",
-        [0xE8] = "Request: HEARTBEAT",
-        [0xE9] = "Request: KEY_UPDATE",
-        [0xEA] = "Request: GET_ENCAPSULATED_REQUEST",
-        [0xEB] = "Request: DELIVER_ENCAPSULATED_RESPONSE",
-        [0xEC] = "Request: END_SESSION",
-        [0xED] = "Request: GET_CSR",
-        [0xEE] = "Request: SET_CERTIFICATE",
-        [0xEF] = "Request: GET_MEASUREMENT_EXTENSION_LOG",
-        [0xF0] = "Request: SUBSCRIBE_EVENT_TYPES",
-        [0xF1] = "Request: SEND_EVENT",
-        [0xFC] = "Request: GET_KEY_PAIR_INFO",
-        [0xFD] = "Request: SET_KEY_PAIR_INFO",
         [0xFE] = "Request: VENDOR_DEFINED_REQUEST",-- 1.0.2
         [0xFF] = "Request: RESPOND_IF_READY",-- 1.0.2
     
@@ -81,48 +63,29 @@ local reqres_types = {
         [0x02] = "Respond: CERTIFICATE", -- 1.0.2
         [0x03] = "Respond: CHALLENGE_AUTH", -- 1.0.2
         [0x04] = "Respond: VERSION", -- 1.0.2
-        [0x05] = "Respond: CHUNK_SEND_ACK",
-        [0x06] = "Respond: CHUNK_RESPONSE",
-        [0x07] = "Respond: ENDPOINT_INFO",
         [0x60] = "Respond: MEASUREMENTS", -- 1.0.2
         [0x61] = "Respond: CAPABILITIES", -- 1.0.2
-        [0x62] = "Respond: SUPPORTED_EVENT_TYPES",
         [0x63] = "Respond: ALGORITHMS", -- 1.0.2
         [0x64] = "Respond: KEY_EXCHANGE_RSP",
-        [0x65] = "Respond: FINISH_RSP",
-        [0x66] = "Respond: PSK_EXCHANGE_RSP",
-        [0x67] = "Respond: PSK_FINISH_RSP",
-        [0x68] = "Respond: HEARTBEAT_ACK",
-        [0x69] = "Respond: KEY_UPDATE_ACK",
-        [0x6A] = "Respond: ENCAPSULATED_REQUEST",
-        [0x6B] = "Respond: ENCAPSULATED_RESPONSE_ACK",
-        [0x6C] = "Respond: END_SESSION_ACK",
-        [0x6D] = "Respond: CSR",
-        [0x6E] = "Respond: SET_CERTIFICATE_RSP",
-        [0x6F] = "Respond: MEASUREMENT_EXTENSION_LOG",
-        [0x70] = "Respond: SUBSCRIBE_EVENT_TYPES_ACK",
-        [0x71] = "Respond: EVENT_ACK",
-        [0x7C] = "Respond: KEY_PAIR_INFO",
-        [0x7D] = "Respond: SET_KEY_PAIR_INFO_ACK",
         [0x7E] = "Respond: VENDOR_DEFINED_RESPONSE",-- 1.0.2
         [0x7F] = "Respond: ERROR"
 }
 
 
-Major      = ProtoField.uint8("Major", "Major Version", base.DEC, NULL, 0xF0)
-Minor      = ProtoField.uint8("Minor", "Minor Version", base.DEC, NULL, 0xF)
-ReqRes     = ProtoField.uint8("ReqRes", "Request Response Code", base.HEX, reqres_types)
-Param_1    = ProtoField.uint8("Param_1", "Parameter 1")
-Param_2    = ProtoField.uint8("Param_2", "Parameter 2")
+Major     = ProtoField.uint8("Major", "Major Version", base.DEC, NULL, 0xF0)
+Minor     = ProtoField.uint8("Minor", "Minor Version", base.DEC, NULL, 0xF)
+ReqRes    = ProtoField.uint8("ReqRes", "Request Response Code", base.HEX, reqres_types)
+Param1   = ProtoField.uint8("Param1", "Parameter 1")
+Param2   = ProtoField.uint8("Param2", "Parameter 2")
 
-Payload    = ProtoField.bytes("Payload", "Payload")
+Payload   = ProtoField.bytes("Payload", "Payload")
+Reserved  = ProtoField.bytes("Reserved", "Reserved ")
 
-Reserved   = ProtoField.bytes("Reserved", "Reserved ")
-VNumCount  = ProtoField.uint8("VNumCount", "Version Number Count")
-MajorV     = ProtoField.uint8("MajorV", "Major Version", base.HEX, NULL, 0xF0)
-MinorV     = ProtoField.uint8("Minorv", "Minor Version", base.HEX, NULL, 0xF)
-UVNum      = ProtoField.uint8("UVNum","Update Version Number", base.HEX, NULL, 0xF0)
-Alpha      = ProtoField.uint8("Alpha", "Alpha", base.HEX, NULL, 0xF)
+VNumCount = ProtoField.uint8("VNumCount", "Version Number Count")
+MajorV    = ProtoField.uint8("MajorV", "Major Version", base.HEX, NULL, 0xF0)
+MinorV    = ProtoField.uint8("Minorv", "Minor Version", base.HEX, NULL, 0xF)
+UVNum     = ProtoField.uint8("UVNum","Update Version Number", base.HEX, NULL, 0xF0)
+Alpha     = ProtoField.uint8("Alpha", "Alpha", base.HEX, NULL, 0xF)
 
 CTExp = ProtoField.uint8("CTExp", "CT Expoent")
 
@@ -134,11 +97,12 @@ local MSCAP = {
 }
 
 
-CACHE_CAP = ProtoField.uint8("CACHE_CAP", "Supports Negotiated State Caching", base.DEC, yesno_types, 0x1)
-CERT_CAP = ProtoField.uint8("CERT_CAP", "Supports GET_DIGESTS and GET_CERTIFICATE", base.DEC, yesno_types, 0x2)
-CHAL_CAP = ProtoField.uint8("CHAL_CAP", "Supports CHALLANGE message", base.DEC, yesno_types, 0x4)
-MEAS_CAP = ProtoField.uint8("MEAS_CAP", "MEASUREMENT Capabilities", base.DEC, MSCAP, 0x18)
+CACHE_CAP      = ProtoField.uint8("CACHE_CAP", "Supports Negotiated State Caching", base.DEC, yesno_types, 0x1)
+CERT_CAP       = ProtoField.uint8("CERT_CAP", "Supports GET_DIGESTS and GET_CERTIFICATE", base.DEC, yesno_types, 0x2)
+CHAL_CAP       = ProtoField.uint8("CHAL_CAP", "Supports CHALLANGE message", base.DEC, yesno_types, 0x4)
+MEAS_CAP       = ProtoField.uint8("MEAS_CAP", "MEASUREMENT Capabilities", base.DEC, MSCAP, 0x18)
 MEAS_FRESH_CAP = ProtoField.uint8("MEAS_FRESH_CAP", "???", base.DEC, yesno_types, 0x20)
+
 
 local BSymAlgo = {
     [1] = "TPM_ALG_RSASSA_2048",
@@ -152,6 +116,7 @@ local BSymAlgo = {
     [256] = "TPM_ALG_ECDSA_ECC_NIST_P521"
 }
 
+
 local BHshAlgo = {
     [1] = "TPM_ALG_SHA_256",
     [2] = "TPM_ALG_SHA_384",
@@ -161,19 +126,28 @@ local BHshAlgo = {
     [32] = "TPM_ALG_SHA3_512"
 }
 
-Length = ProtoField.uint16("Length", "Length", base.DEC) 
-MSpecs = ProtoField.uint8("MSpecs", "Measurement Specification")
-SymAlg = ProtoField.uint32("SymAlg", "Supported key signature algorithms", base.DEC, BSymAlgo)
-HshAlg = ProtoField.uint32("HshAlg", "Supported hashing algorithms", base.DEC, BHshAlgo)
-AsyC = ProtoField.uint8("AsyC", "Number of supported key algorithms")
-HshC = ProtoField.uint8("HshC", "Number of supported hashing algorithms")
-Asym = ProtoField.uint32("Asym", "Supported key algorithm")
-Hsh = ProtoField.uint32("Hsh", "Supported hashing algorithm")
+local AlgTypes = {
+    [2] = "DHE",
+    [3] = "AEADCipherSuite",
+    [4] = "ReqBaseAsymAlg",
+    [5] = "KeySchedule"
+}
 
-AlgType = ProtoField.bytes("AlgType", "Algorithm Type")
-AlgCount = ProtoField.bytes("AlgCount", "Number of supported algorithms")
-AlgSup = ProtoField.bytes("AlgSup", "Supported algorithms")
-AlgExt = ProtoField.bytes("AlgExt", "Extended supported algorithms")
+
+Length   = ProtoField.uint16("Length", "Length", base.DEC) 
+MSpecs   = ProtoField.uint8("MSpecs", "Measurement Specification")
+SymAlg   = ProtoField.uint32("SymAlg", "Supported key signature algorithms", base.DEC, BSymAlgo)
+HshAlg   = ProtoField.uint32("HshAlg", "Supported hashing algorithms", base.DEC, BHshAlgo)
+AsyC     = ProtoField.uint8("AsyC", "Number of supported key algorithms")
+HshC     = ProtoField.uint8("HshC", "Number of supported hashing algorithms")
+Asym     = ProtoField.uint32("Asym", "Supported key algorithm")
+Hsh      = ProtoField.uint32("Hsh", "Supported hashing algorithm")
+
+AlgType  = ProtoField.uint8("AlgType", "Algorithm Type", base.HEX, AlgTypes)
+AlgSup   = ProtoField.bytes("AlgSup", "Supported algorithms")
+AlgExt   = ProtoField.bytes("AlgExt", "Extended supported algorithms")
+EAlgCount = ProtoField.uint8("ExtAlgCount", "Number of extended supported algorithms", base.DEC, NULL, 15)
+FAlgCount = ProtoField.uint8("FixedAlgCount", "Number of fixed supported algorithms", base.DEC, NULL, 240)
 
 MHshAlg = ProtoField.uint32("MHshAlg", "Supported hashing algorithms")
 
@@ -185,8 +159,8 @@ spdm.fields = {
     Major,
     Minor,
     ReqRes,
-    Param_1,
-    Param_2,
+    Param1,
+    Param2,
     Payload,
 
     -- Version --
@@ -226,9 +200,10 @@ spdm.fields = {
     Hsh,
 
     AlgType,
-    AlgCount,
     AlgSup,
     AlgExt,
+    EAlgCount,
+    FAlgCount,
 
     -- Algorithms --
     -- MSpecsSel --
@@ -285,8 +260,8 @@ function spdm.dissector(buffer, pinfo, tree)
             local p1 = buffer(header_length + 7, 1)
             local p2 = buffer(header_length + 8, 1)
 
-            subtree_2:add(Param_1, p1)
-            subtree_2:add(Param_2, p2)
+            subtree_2:add(Param1, p1)
+            subtree_2:add(Param2, p2)
 
             local spdm_length = length - 9 - 4
 
@@ -306,19 +281,12 @@ function spdm.dissector(buffer, pinfo, tree)
             elseif info == 0xE1 then
                 pinfo.cols.info = "Request: GET_CAPABILITIES"
                 return
-                --local get_cap = subtree_2:add(spdm, buffer(begin, 8), "Version Message")
 
-                --get_cap:add(Reserved, buffer(begin, 1))
-                --get_cap:add(CTExp, buffer(begin + 1, 1))
-                --get_cap:add(Reserved, buffer(begin + 2 2))
-                --get_cap:add(Flags, buffer(begin + 4, 4))
-                --get_cap:add(TransSize, buffer(begin + 8, 4))
-                --get_cap:add(MaxSize, buffer(begin + 12, 4))
-                --get_cap:add(Sup, buffer(begin + 16, 1))
 
             elseif info == 0xE3 then
                 pinfo.cols.info = "Request: NEGOTIATE_ALGORITHMS"
-                local n = (buffer(begin + 1, 1) .. buffer(begin, 1)):uint()
+
+                local n = tonumber(buffer(begin + 1, 1) .. buffer(begin, 1), 16)
 
                 local neg_alg = subtree_2:add(spdm, buffer(begin, n - 4), "Negotiate Algorithms Message")
 
@@ -354,33 +322,32 @@ function spdm.dissector(buffer, pinfo, tree)
                 end
                 
                 local struct_beg = begin + 28 + 4*A + 4*E
-                               
                 local trees = {}
 
                 i = 0
-                local algC = buffer(struct_beg + 1 + i, 1):uint()
+                local algStructSize = n - 32 - 4*E - 4*A
 
-                do
-                    repeat
-                        local ExtAlgCount = algC and 0b00001111
-                        local FixedAlgCount = algC and 0b11110000
-    
-                        tress[i] = neg_alg:add(spdm, buffer(struct_beg, 4*ExtAlgCount + FixedAlgCount + 2), "Algorithm Request Structure")
-    
-                        trees[i]:add(AlgType, buffer(struct_beg, 1))
-                        trees[i]:add(AlgCount, buffer(struct_beg + 1, 1))
-                        trees[i]:add(AlgSup, buffer(struct_beg + 2, FixedAlgCount))
-                        trees[i]:add(AlgExt, buffer(struct_beg + 2 + FixedAlgCount, 4*ExtAlgCount))
+                while i ~= algStructSize do
+                    local algC = buffer(struct_beg + 1 + i, 1):uint()
 
-                        i = i + 1
+                    local ExtAlgCount = bit.band(algC, 15)
+                    local FixedAlgCount = bit.rshift(bit.band(algC, 240), 4)
 
-                        if i == p1 then
-                            break
-                        end
-                        algC = buffer(struct_beg + 1 + i*(1 + FixedAlgCount + 4*ExtAlgCount), 1):uint()
-                    until true
+                    trees[i] = neg_alg:add(spdm, buffer(struct_beg + i, 2 + FixedAlgCount + 4*ExtAlgCount), "Algorithm Request")
+
+                    trees[i]:add(AlgType, buffer(struct_beg + i, 1))
+                    trees[i]:add(FAlgCount, buffer(struct_beg + i + 1, 1))
+                    trees[i]:add(EAlgCount, buffer(struct_beg + i + 1, 1))
+                    trees[i]:add(AlgSup, buffer(struct_beg + i + 2, FixedAlgCount))
+
+                    if ExtAlgCount ~= 0 then
+                        trees[i]:add(AlgExt, buffer(struct_beg + i + 2 + FixedAlgCount, 4*ExtAlgCount))
+                    else
+                        trees[i]:add(AlgExt, "None")
+                    end
+
+                    i = i + 2 + FixedAlgCount + 4*ExtAlgCount
                 end
-                
 
             elseif info == 0xFF then
                 pinfo.cols.info = "Request: RESPOND_IF_READY"
@@ -443,42 +410,69 @@ function spdm.dissector(buffer, pinfo, tree)
             elseif info == 0x63 then
                 pinfo.cols.info = "Respond: ALGORITHMS"
 
-                local n = buffer(begin, 2):uint()
+                local n = tonumber(buffer(begin + 1, 1) .. buffer(begin, 1), 16)
 
-                local neg_alg = subtree_2:add(spdm, buffer(begin, n), "Algorithms Message")
+                local alg = subtree_2:add(spdm, buffer(begin, n - 4), "Negotiate Algorithms Message")
 
-                neg_alg:add(Length, buffer(begin, 2))
-                neg_alg:add(MSpecs, buffer(begin + 2, 1))
-                neg_alg:add(Reserved, buffer(begin + 3, 1))
-                neg_alg:add(MHshAlg, buffer(begin + 3, 4))
+                alg:add(Length, n)
+                alg:add(MSpecs, buffer(begin + 2, 1))
+                alg:add(Reserved, buffer(begin + 3, 1))
+                alg:add(MHshAlg, buffer(begin + 4, 4))
                 
-                local hash = neg_alg:add(spdm, buffer(begin + 11, 10), "Hashing Algorithms")
+                
+                alg:add(SymAlg, buffer(begin + 8, 4))
+                alg:add(HshAlg, buffer(begin + 12, 4))
+                alg:add(Reserved, buffer(begin + 16, 12))
+                
+                alg:add(AsyC, buffer(begin + 28, 1))
+                alg:add(HshC, buffer(begin + 29, 1))
 
-                local asym = neg_alg:add(spdm, buffer(begin + 7, 10), "Asymmetric key Algorithms")
+                local A = buffer(begin + 28, 1):uint()
+                local E = buffer(begin + 29, 1):uint()
 
+                alg:add(Reserved, buffer(begin + 30, 2))
 
-                asym:add(BSymAlgo, buffer(begin + 7, 4))
-                asym:add(AsyC, buffer(begin + 23, 1))
-                local A = buffer(begin + 23, 1):uint()
+                local asymL = alg:add(spdm, buffer(begin + 32, 4*A), "List of Asymmetric Algorithms")
+                local hashL = alg:add(spdm, buffer(begin + 32 + 4*A, 4*E), "List of Hashing Algorithms")
 
-                local asym_list = asym:add(spdm, buffer(begin + 27, 4*A), "List")
-                local i = 0
+                local i
+                    
+                if A ~= 0 then
+                    for i = 0, 4*A, 4 do
+                        asymL:add(Asym, buffer(begin + 32 + i, 4))
+                    end
 
-                while i < 4*A do
-                    asym_list:add(Asym, buffer(begin + 27 + i, 4))
-                    i = i + 4
+                    for i = 0, 4*E, 4 do
+                        hashL:add(Hsh, buffer(begin + 36 + 4*A + i, 4))
+                    end
                 end
+                
+                local struct_beg = begin + 32 + 4*A + 4*E
+                local trees = {}
 
-                hash:add(BHshAlgo, buffer(begin + 12, 4))
-                hash:add(HshC, buffer(begin + 29, 1))
-                local E = buffer(begin + 28, 1):uint()
-
-                local hsh_list = hash:add(spdm, buffer(begin + 27 + 4*A, 4*E))
                 i = 0
+                local algStructSize = n - 36 - 4*E - 4*A
 
-                while i < 4*E do
-                    hsh_list:add(Hsh, buffer(begin + 27 + 4*A + i, 4))
-                    i = i + 4
+                while i ~= algStructSize do
+                    local algC = buffer(struct_beg + 1 + i, 1):uint()
+
+                    local ExtAlgCount = bit.band(algC, 15)
+                    local FixedAlgCount = bit.rshift(bit.band(algC, 240), 4)
+
+                    trees[i] = alg:add(spdm, buffer(struct_beg + i, 2 + FixedAlgCount + 4*ExtAlgCount), "Algorithm Request")
+
+                    trees[i]:add(AlgType, buffer(struct_beg + i, 1))
+                    trees[i]:add(FAlgCount, buffer(struct_beg + i + 1, 1))
+                    trees[i]:add(EAlgCount, buffer(struct_beg + i + 1, 1))
+                    trees[i]:add(AlgSup, buffer(struct_beg + i + 2, FixedAlgCount))
+
+                    if ExtAlgCount ~= 0 then
+                        trees[i]:add(AlgExt, buffer(struct_beg + i + 2 + FixedAlgCount, 4*ExtAlgCount))
+                    else
+                        trees[i]:add(AlgExt, "None")
+                    end
+
+                    i = i + 2 + FixedAlgCount + 4*ExtAlgCount
                 end
 
 
