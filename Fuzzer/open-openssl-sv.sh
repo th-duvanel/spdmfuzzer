1c1227e075f8124a5b2ac0cfa0048c795af9fc4b
Thiago Duvanel <thiago.duvanel@usp.br>
2024-06-06 13:55:47 -0300

    Organizing files and fuzzers deps

--- Fuzzer/open-openssl-sv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ ! -f cert.pem ]] && [[ ! -f key.pem ]]; then
-    openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem -subj /C=/ST=/L=/O=/OU=/CN=
-fi
-
-openssl s_server -key key.pem -dkey key.pem -cert cert.pem -dcert cert.pem
--- Fuzzer/tlsfuzzer-deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#PYTHONPATH=tlsfuzzer python3 tlsfuzzer/scripts/test-fuzzed-plaintext.py
-
-pip3 install --pre tlslite-ng
-pip3 install gmpy2
