134f3f56e1a4f3d7b505eca7ca3746aa4fc02591
Thiago Duvanel <thiago.duvanel@usp.br>
2024-06-09 21:22:34 -0300

    libspdm: certs: updated certs based on last version

--- Fuzzer/local/compile.sh
+++ Fuzzer/local/compile.sh
@@ -5,14 +5,15 @@ cd spdm-emu
 git checkout 8d5a74e5b8dc8618556df0c5c81f76ddda8a5727
 git submodule update --init --recursive
 
-mv ../files/spdm-emu/CMakeLists.txt ./
-mv ../files/libspdm/CMakeLists.txt libspdm/
+cp ../files/spdm-emu/CMakeLists.txt ./
+cp ../files/libspdm/CMakeLists.txt libspdm/
+cp -r ../files/libpsdm/unit_test/sample_key/ libspdm/unit_test/sample_key/
 
 mkdir build
 cd build
-cmake -DARCH=x64 -DTOOLCHAIN=GCC -DTARGET=Release -DCRYPTO=mbedtls ..
+cmake -DARCH=x64 -DTOOLCHAIN=GCC -DTARGET=Debug -DCRYPTO=mbedtls ..
 make copy_sample_key
 make
 
-ln -s ./spdm-emu/build/bin/spdm_responder_emu spdm_responder_emu
-ln -s ./spdm-emu/build/bin/spdm_requester_emu spdm_requester_emu
+cd ../../
+
