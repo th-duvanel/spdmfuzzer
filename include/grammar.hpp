2f7350c2020d54c7368fdfe989bf3402961d433b
Thiago Duvanel <thiago.duvanel.ferreira@live.com>
2024-07-20 22:21:58 -0300

    grammar: added capabilities support

--- include/grammar.hpp
+++ include/grammar.hpp
@@ -10,18 +10,16 @@
 extern u32 finishCommand;
 
 /**
+ * @brief Global variable which represents the size of Measurements Hash size algorithm message
+ */
+extern u8  M;
+
+/**
  * @brief Possible Request Response codes from SPDM
  * 
  * This map contains the possible Request Response codes from SPDM, from a string (req res name to the number)
  */
 extern std::map<std::string, u8> RequestResponseCode;
-
-/**
- * @brief Internal packets grammar structure
- * 
- * This map contains the internal packets grammar structure, with the packet name and its fields.
- */
-extern std::map<std::string, std::vector<std::string>> packetStructure;
 
 /**
  * @class responsePacket
@@ -30,54 +28,56 @@ extern std::map<std::string, std::vector<std::string>> packetStructure;
  * This class is the base class for all response packets grammar, containing the common fields and methods.
  */
 class responsePacket {
-    protected:
-        u32  size;  ///< Size of the packet
+protected:
+    u32  size;  ///< Size of the packet
 
-        u8   SPDM;  ///< SPDM version
-        u8   major_and_minor; ///< Major and minor version
-        u8   reqresCode; ///< Request Response code
-        u8   param1; ///< First parameter
-        u8   param2; ///< Second parameter
+    u8   SPDM;  ///< SPDM version
+    u8   major_and_minor; ///< Major and minor version
+    u8   reqresCode; ///< Request Response code
+    u8   param1; ///< First parameter
+    u8   param2; ///< Second parameter
 
-        /**
-         * @brief Constructor for the responsePacket class
-         * 
-         * This constructor initializes the responsePacket class with the common fields.
-         * 
-         * @param reqresCode Request Response code
-         * @param param1 First parameter
-         * @param param2 Second parameter
-         */
-        responsePacket(u8 reqresCode, u8 param1, u8 param2);
+    /**
+     * @brief Constructor for the responsePacket class
+     * 
+     * This constructor initializes the responsePacket class with the common fields.
+     * 
+     * @param reqresCode Request Response code
+     * @param param1 First parameter
+     * @param param2 Second parameter
+     */
+    responsePacket(u8 reqresCode, u8 param1, u8 param2);
 
-        /**
-         * @brief Serializes the header of the packet
-         * 
-         * This method serializes the header of the packet, returning a buffer with the serialized data.
-         * 
-         * @return u8* Buffer with the serialized data
-         */
-        u8* serializeHeader();
+    /**
+     * @brief Serializes the header of the packet
+     * 
+     * This method serializes the header of the packet, returning a buffer with the serialized data.
+     * 
+     * @return u8* Buffer with the serialized data
+     */
+    u8* serializeHeader();
 
-    public:
+public:
 
-        /**
-         * @brief Serializes the packet
-         * 
-         * This method serializes the packet, returning a void pointer with the serialized data.
-         * 
-         * @return void* Buffer with all the serialized data
-         */
-        virtual void* serialize() = 0;
+    /**
+     * @brief Serializes the packet
+     * 
+     * This method serializes the packet, returning a void pointer with the serialized data.
+     * 
+     * @param max Max size of the packet. If is set to zero or none, it will not use random size.
+     * 
+     * @return void* Buffer with all the serialized data
+     */
+    virtual void* serialize(size_t max = 0) = 0;
 
-        /**
-         * @brief Gets the size of the packet
-         * 
-         * This method returns the size of the packet.
-         * 
-         * @return u32 Size of the packet
-         */
-        u32 getSize();
+    /**
+     * @brief Gets the size of the packet
+     * 
+     * This method returns the size of the packet.
+     * 
+     * @return u32 Size of the packet
+     */
+    u32 getSize();
 };
 
 /**
@@ -87,35 +87,62 @@ class responsePacket {
  * This class represents the Version response packet grammar, containing the fields and methods to serialize the packet.
  */
 class Version : public responsePacket {
-    private:
-        u8   reserved;   ///< Reserved field
-        u8   entryCount; ///< Entry count
-        u16* entry;      ///< Entry field (2*entryCount size)
+private:
+    u8   reserved;   ///< Reserved field
+    u8   entryCount; ///< Entry count
 
-    public:
-        /**
-         * @brief Constructor for the Version class
-         * 
-         * This constructor initializes the Version class with the common fields and the specific fields for the Version packet.
-         * 
-         * @param random_size If the size of the packet should be randomized
-         */
-        Version(bool random_size = false);
+    struct ver_number {
+        u8 major_version;  ///< Major version
+        u8 minor_version;  ///< Minor version
+        u8 update_version; ///< Update version
+        u8 alpha;          ///< Alpha version
+    } *entry;
 
-        /**
-         * @brief Destructor for the Version class
-         * 
-         * This destructor deallocates the entry field.
-         */
-        virtual ~Version();
+public:
+    /**
+     * @brief Constructor for the Version class
+     * 
+     * This constructor initializes the Version class with the common fields and the specific fields for the Version packet.
+     */
+    Version();
 
-        /**
-         * @brief Serializes the packet
-         * 
-         * This method serializes the packet, returning a void pointer with the serialized data.
-         * 
-         * @return void* Buffer with all the serialized data
-         */
-        void* serialize() override;
+    /**
+     * @brief Destructor for the Version class
+     * 
+     * This destructor deallocates the entry field.
+     */
+    virtual ~Version();
+
+    /**
+     * @brief Serializes the packet
+     * 
+     * This method serializes the packet, returning a void pointer with the serialized data.
+     * 
+     * @param max Max size of the packet. If is set to zero or none, it will not use random size.
+     * 
+     * @return void* Buffer with all the serialized data
+     */
+    void* serialize(size_t max = 0) override;
+};
+
+
+class Capabilities : public responsePacket {
+private:
+    u8  reserved;    ///< Reserved field
+    u8  ct_exponent; ///< CT Exponent field
+    u16 reserved_2;  ///< Reserved field
+    
+    struct flags {
+        u8 cache_cap;
+        u8 cert_cap;
+        u8 chal_cap;
+        u8 meas_cap;
+        u8 meas_fresh_cap;
+    } flags;
+
+public:
+    Capabilities();
+
+    void* serialize(size_t max = 0) override;
 };
 
--- src/grammar.cpp
+++ src/grammar.cpp
@@ -2,6 +2,8 @@
 
 u32 finishCommand = (0x00 << 24) | (0x00 << 16) | (0xff << 8) | 0xfe;
 
+u8  M;
+
 // To make the code more readable, we can define the namespaace right before coding.
 
 inline std::map<std::string, u8> RequestResponseCode = {
@@ -20,21 +22,11 @@ inline std::map<std::string, u8> RequestResponseCode = {
 
 // The real packet structure is stored in each packet class.
 
-// The idea here is to create a map that will store the structure of parts of the packet,
+// The idea here is to create a class that will store the structure of parts of the packet,
 // until we reach a point where we can really write a random number without
 // compromissing the packet structure.
 
-inline std::map<std::string, std::vector<std::string>> packetStructure = {
-    // VERSION
-    {"VersionNumberEntry", {"major-version", "minor-version", "update-version", "alpha"}},
-    // CAPABILITIES
-    {"Flags", {"cache_cap", "cert_cap", "meas_cap", "meas_fres_cap", "reserved", "reserved", "reserved", "reserved"}},
-    // ALGORITHMS
-    {"MeasurementHashAlgo", {"raw-only", "sha_256", "sha_384", "sha_512", "sha3_256", "sha3_384", "sha3_512"}}
-    // TODO
-};
 
-// TODO: could construct with member init? (ugly)
 responsePacket::responsePacket(u8 reqresCode, u8 param1, u8 param2)
 {
     this->size = 5;
@@ -65,18 +57,20 @@ u32 responsePacket::getSize()
 }
 
 
-Version::Version(bool random_size) : responsePacket(RequestResponseCode["VERSION"], 0, 0)
+Version::Version() : responsePacket(RequestResponseCode["VERSION"], 0, 0)
 {
-    // ToDo: make random_size
-    this->reserved = 0;
+    this->reserved   = randomize(0, 255);
+    // For SpdmRequester, this must be between 0 and 2.
     this->entryCount = randomize(0, 2);
-    this->size += 2 + (this->entryCount * 2);
-    this->entry = new u16[this->entryCount];
+    // this->entryCount = randomize(0, 255);
+    this->size      += 2 + (this->entryCount * 2);
+    this->entry      = new ver_number[this->entryCount];
 
     for(u8 i = 0 ; i < this->entryCount ; i++) {
-        u8 VERSION_major_and_minor = (randomize(0, 15) << 4) | randomize(0, 15);
-        u8 VERSION_update_and_alpha = (randomize(0, 15) << 4) | randomize(0, 15);
-        this->entry[i] = (VERSION_major_and_minor << 4) | VERSION_update_and_alpha;
+        this->entry[i].major_version  = randomize(0, 15);
+        this->entry[i].minor_version  = randomize(0, 15);
+        this->entry[i].update_version = randomize(0, 15);
+        this->entry[i].alpha          = randomize(0, 15);
     }
 }
 
@@ -85,9 +79,11 @@ Version::~Version()
     delete[] this->entry;
 }
 
-void* Version::serialize()
+void* Version::serialize(size_t max)
 {
-    u8* buffer = new u8[7 + (this->entryCount * 2)];
+    this->size += randomize(0, max);
+
+    u8* buffer = new u8[this->size];
 
     u8* header = this->serializeHeader();
     std::memcpy(buffer, header, 4);
@@ -96,8 +92,55 @@ void* Version::serialize()
     buffer[6] = this->entryCount;
 
     for(u8 i = 0 ; i < this->entryCount ; i++) {
-        buffer[7 + (i * 2)] = this->entry[i] >> 8;
-        buffer[8 + (i * 2)] = this->entry[i];
+        buffer[7 + (i * 2)] = this->entry[i].update_version << 4 | this->entry[i].alpha;
+        buffer[8 + (i * 2)] = this->entry[i].major_version << 4 | this->entry[i].minor_version;
+    }
+
+    // Fill in with random numbers
+    for(u8 i = 7 + (this->entryCount * 2) ; i < this->size ; i++) {
+        buffer[i] = randomize(0, 255);
+    }
+
+    delete[] header;
+    return buffer;
+}
+
+
+
+Capabilities::Capabilities() : responsePacket(RequestResponseCode["CAPABILITIES"], 0, 0)
+{
+    this->reserved = randomize(0, 255);
+    this->ct_exponent = randomize(0, 255);
+    this->reserved_2 = randomize(0, 65535);
+
+    this->flags.cache_cap = randomize(0, 1);
+    this->flags.cert_cap = randomize(0, 1);
+    this->flags.chal_cap = randomize(0, 1);
+    this->flags.meas_cap = randomize(0, 3);
+    this->flags.meas_fresh_cap = randomize(0, 1);
+
+    this->size += 8;
+}
+
+void* Capabilities::serialize(size_t max)
+{
+    this->size += randomize(0, max);
+
+    u8* buffer = new u8[this->size];
+
+    u8* header = this->serializeHeader();
+    std::memcpy(buffer, header, 4);
+
+    buffer[5] = this->reserved;
+    buffer[6] = this->ct_exponent;
+    buffer[7] = this->reserved_2 >> 8;
+    buffer[8] = this->reserved_2 & 0xff;
+
+    buffer[9] = this->flags.meas_fresh_cap << 5 | this->flags.meas_cap << 3 | this->flags.chal_cap << 2 | this->flags.cert_cap << 1 | this->flags.cache_cap;
+
+    // Fill in with random numbers
+    for(u8 i = 10 ; i < this->size ; i++) {
+        buffer[i] = randomize(0, 255);
     }
 
     delete[] header;
