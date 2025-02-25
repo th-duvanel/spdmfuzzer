27b628de19084d8b4a48916eea0239e1369a180a
Thiago Duvanel <thiago.duvanel.ferreira@live.com>
2025-02-25 20:17:29 -0300

    observer: feat: removed server & client hello prints (they're mocked)

--- src/observer.cpp
+++ src/observer.cpp
@@ -30,6 +30,7 @@ void ConsoleLogger::onResponse(MessageSPDM &Message, u8 fuzzStrategy)
     int msgCode = Message.getCode();
     // Removes CAPABILITIES AND GET_CAPABILITIES, VERSION, GET_VERSION
     if (msgCode == 0 || msgCode == 0x84 || msgCode == 0x04 || msgCode == 0xE1 || msgCode == 0x61 || msgCode == 0xE3) return;
+    if (Message.Command == 2917007360) return;
     
     std::cout << "# [" << (int)fuzzStrategy << "] => " << ResponseRequestCode[Message.getCode()];
     std::stringstream ss;
